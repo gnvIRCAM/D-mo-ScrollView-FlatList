c329884b95de7b265d58c9b62ca57dcd1f7cb6c6
genova <genovadavid44@gmail.com>
2024-03-08 17:57:12 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,18 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+function fakeUsers(n_users) {
+  // To generate quickly a large number of "users"
+  var users = new Array();
+  for (i=0; i<=n_users; ++i) {
+    users.push({
+      'username':i, 
+      'first_name': Math.floor(Math.random()*1e6),
+      'last_name': Math.floor(Math.random()*1e6),});
+  }
+  return users;
+}
 
 export default function App() {
+  // const userData = require('./assets/users.json'); // final.json
+  const userData = fakeUsers(2000); // List of fake users
+  const [users, setUsers] = useState(userData);
+  const [display, setDisplay] = useState(true); 
+  const [textInput, setTextInput] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+        onChangeText={setTextInput} 
+        placeholder='Uncontrolled'
+        />
+      <TextInput 
+        value={textInput}
+        onChangeText={setTextInput} 
+        placeholder='Controlled'
+        />
+      <Button onPress={() => setDisplay(!display)} title='Display' />
+      {/* {display && 
+      <ScrollView>
+        {users.map((user) => <Text key={user.username}>
+          User {user.username}, Name: {user.first_name}, 
+        </Text>)}
+      </ScrollView>} */}
+      {display && 
+      <FlatList
+        data={users} // liste de données
+        keyExtractor={item => item.username} // mapping élément -> id
+        renderItem={({item}) => 
+        <Text>
+          User : {item.username},
+          Name : {item.first_name}
+        </Text>}
+      />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    margin: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
